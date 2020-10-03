8af9a23d85294394a2a13bf59012bd873c50b7b4
khalid <khalid.ibr212@gmail.com>
2020-10-03 20:06:01 +0300

    Updates

---
+++ precache-manifest.dd0562f4e6a0a28fb0b2308637ed7156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64baf560129088400efd76ecc22d162e",
+    "url": "/http//Khalid-212.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "b52b86d89812635cc6af",
+    "url": "/http//Khalid-212.github.io/portfolio/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "a9b241c18c76fe2d06e8",
+    "url": "/http//Khalid-212.github.io/portfolio/static/css/main.29aece9f.chunk.css"
+  },
+  {
+    "revision": "b52b86d89812635cc6af",
+    "url": "/http//Khalid-212.github.io/portfolio/static/js/2.675aa9c1.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/http//Khalid-212.github.io/portfolio/static/js/2.675aa9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b241c18c76fe2d06e8",
+    "url": "/http//Khalid-212.github.io/portfolio/static/js/main.568464c8.chunk.js"
+  },
+  {
+    "revision": "5b80f46ed99fe6665377",
+    "url": "/http//Khalid-212.github.io/portfolio/static/js/runtime-main.29f72985.js"
+  }
+]);
